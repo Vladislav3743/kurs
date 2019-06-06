4a06589c8bce0ac073c41fcc4506306ffd96cf54
vlad <Vlados.tang@mail.ru>
2019-06-06 14:54:53 +0300

    I create main.php file, style dir

---
+++ main.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Главная страница</title>
+	<link rel="stylesheet" type="text/css" href="./style/main.css">
+</head>
+<body>
+	<header>
+		<h1>Список ваших задач</h1>
+	</header>
+	<section>
+		<div class="tasks">
+			<p id="tasks">
+				
+			</p>
+		</div>
+	</section>
+</body>
+</html>
